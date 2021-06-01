338e8401e55b7356986475c7bac1ed8a9e69adc2
Raphaël Carré <raphaelcarre@me.com>
2021-06-02 00:28:24 +0200

    Sauce model correction

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose'
 
 const sauceSchema = mongoose.Schema({
-    userId:         { type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    userId:         { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     name:           { type: String, required: true },
     manufacturer:   { type: String, required: true },
     description:    { type: String, required: true },
     mainPepper:     { type: String, required: true },
     imageUrl:       { type: String, required: true },
     heat:           { type: Number, required: true },
-    likes:          { type: Number },
-    dislikes:       { type: Number },
+    likes:          { type: Number, default: 0 },
+    dislikes:       { type: Number, default: 0 },
     usersLiked:     { type: [String] },
     usersDisliked:  { type: [String] }
 })
