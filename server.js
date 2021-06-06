4979175aaf9c7cc279865ad90211963c268d4b04
Raphaël Carré <raphaelcarre@me.com>
2021-06-06 18:24:56 +0200

    Adding error handling in server.js

--- server.js
+++ server.js
@@ -12,6 +12,27 @@ const port = normalizePort(process.env.PORT || '3000')
 
 const server = http.createServer(app)
 
+const errorHandler = error => {
+    if (error.syscall !== 'listen') throw error
+
+    const address = server.address()
+    const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`
+
+    switch (error.code) {
+        case 'EACCES':
+            logger.error(`${bind} requires elevated privileges.`);
+            process.exit(1)
+            break
+        case 'EADDRINUSE':
+            logger.error(`${bind} is already in use.`)
+            process.exit(1)
+            break
+        default:
+            throw error
+    }
+}
+
+server.on('error', errorHandler)
 server.on('listening', () => {
     logger.info(`Server started on http://localhost:${port}`)
 })
