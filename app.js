4d9238e09391092bf86be9174b022a19b706b1e8
Raphaël Carré <raphaelcarre@me.com>
2021-06-06 19:11:07 +0200

    Adding CORS environement variable

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ dotenv.config({ path: './.env' })
 mongoDb.connection()
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN)
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
     next()
