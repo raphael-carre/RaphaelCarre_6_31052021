8d45b9507d4311313b5d7e69e3c626f6fc691641
Raphaël Carré <raphaelcarre@me.com>
2021-06-15 14:46:39 +0200

    FetchErrorHandler.js implementation

---
+++ config/FetchErrorHandler.js
@@ -0,0 +1,45 @@
+/**
+ * FetchErrorHandler Class
+ */
+class FetchErrorHandler extends Error {
+    /**
+     * @param {Number} statusCode 
+     * @param {String} errorMessage 
+     */
+    constructor(statusCode = 500, errorMessage = null) {
+        super()
+
+        this.statusCode = statusCode || 400
+        this.message = errorMessage || this.message(statusCode)
+    }
+
+    /**
+     * Returns an error message depending on statusCode parameter
+     * @param {Number} statusCode 
+     * @returns {String} Error message
+     */
+    message(statusCode) {
+        let errorMessage
+
+        switch (statusCode) {
+            case 400:
+                errorMessage = 'Requête invalide !'
+                break
+            case 401:
+                errorMessage = 'Vous n\'avez pas l\'autorisation d\'effectuer cette action !'
+                break
+            case 403:
+                errorMessage = 'Accès refusé !'
+                break
+            case 404:
+                errorMessage = 'Ressource introuvable...'
+                break
+            default:
+                errorMessage = 'Il y a eu un problème...'
+        }
+
+        return errorMessage
+    }
+}
+
+export default FetchErrorHandler
